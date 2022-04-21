c7bcffcec4084f711c30300f133e9e39d95838d7
yeyling <74540166+yeyling@users.noreply.github.com>
2022-04-21 15:00:30 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '33d46180929a4513bd8aaf1f9a0cc9bd',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'yeyling Mini Blog',
+  domain: 'https://yeyling.notion.site/yeyling-Notion-33d46180929a4513bd8aaf1f9a0cc9bd',
+  author: 'yeyling',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'Unknown',
+  github: 'Unknown',
+  linkedin: 'Unknown',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
